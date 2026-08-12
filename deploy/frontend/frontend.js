738569f58adee159ed8aaf66e341b6b0678aaa7e
pathywang <pathywang@gmail.com>
2026-08-12 21:20:24 +0100

    update

--- deploy/frontend/frontend.js
+++ deploy/frontend/frontend.js
@@ -3,7 +3,7 @@ const authorElement = document.getElementById("author");
 const button = document.getElementById("new-quote");
 
 async function showQuote() {
-  const response = await fetch("http://127.0.0.1:3000/");
+  const response = await fetch("https://j9g27fgaoooe52gtldtirqy9.trainees.hosting.cyf.academy");
   const quote = await response.json();
 
   quoteElement.textContent = quote.quote;
